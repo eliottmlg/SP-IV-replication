7c2f7fbb4b73685bf6c5526902e71f6229d7a1b0
Eliott Von-Pine <eliott.vonpine@gmail.com>
2024-12-23 20:58:03 +0100

    initial commit

---
+++ Main.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Dec 23 16:32:41 2024
+
+@author: Maxime Coulet, Nina Stizi, Eliott Von-Pine
+"""
+
+#==============================================================================
+# Advanced Macroeconometrics - Final assignement
+# 
+# Dynamic Identification Using System Projections on Instrumental Variables 
+# Working Paper No. 2204 – Dallas Fed
+#==============================================================================
+
